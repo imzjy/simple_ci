ece4531111d65804ac2c83478f32887843ac1c5f
Jerry Chou <imjatsz@gmail.com>
2015-01-04 11:57:50 +0800

    update

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ simple_ci
 =========
 
 very simple continuous integration server, supporting bitbucket.org and github.com webhooks. 
+
+## dependencies
+
+* Flask
+* paramiko
+* qjson
--- simple_ci/simple_ci.py
+++ simple_ci/simple_ci.py
@@ -1,42 +1,61 @@
 # -*- coding: utf-8 -*-
 
-import urllib, json
+import urllib
 import paramiko
+import qjson
 from datetime import datetime
 from flask import Flask, request
- 
+
 app = Flask(__name__)
- 
+
 @app.route("/")
 def hello():
     return ("Simple CI Server: %s" % datetime.now())
  
-@app.route("/ci", methods=["GET", "POST"])
-def ci():
-    if request.method == 'POST':
-        posted_data = request.stream.read()
-        assert posted_data, 'post data is empty'
-        # print posted_data
-        try:
-            web_notice = json.loads(urllib.unquote_plus(posted_data).lstrip('payload='))
-            branch = web_notice['commits'][0]['branch']
-            print branch
-            
-            if branch in ('master', 'online') :
-                cm = web_notice['commits'][0]
-                print "%s %s %s" % (cm['raw_node'], cm['author'], cm['utctimestamp'])
-                print cm['message']
-                deploy(branch)
-        except ex:
-            print ex
-            return 'error'
-        return posted_data
-    return ("Simple CI Server: %s" % datetime.now())
+@app.route("/bitbucket", methods=["POST"])
+def bitbucket():
+    posted_data = request.stream.read()
+    assert posted_data, 'post data is empty'
+    # print posted_data
+    try:
+        push_notice = qjson.loads(urllib.unquote_plus(posted_data).lstrip('payload='))
+        branch = push_notice['commits'][0]['branch']
+        print branch
+        
+        if branch in ('master', 'online') :
+            cm = push_notice['commits'][0]
+            print "%s %s %s" % (cm['raw_node'], cm['author'], cm['utctimestamp'])
+            print cm['message']
+            ssh_script('deploy.sh', branch)
+    except ex:
+        print ex
+        return 'error'
+    return posted_data
+
+@app.route("/github", methods=["POST"])
+def github():
+    posted_data = request.stream.read()
+    assert posted_data, 'post data is empty'
+    # print posted_data
+    try:
+        push_notice = qjson.loads(urllib.unquote_plus(posted_data).lstrip('payload='))
+        branch = push_notice['commits'][0]['branch']
+        print branch
+        
+        if branch in ('master', 'online') :
+            cm = push_notice['commits'][0]
+            print "%s %s %s" % (cm['raw_node'], cm['author'], cm['utctimestamp'])
+            print cm['message']
+            local_script('deploy.sh', branch)
+    except ex:
+        print ex
+        return 'error'
+    return posted_data
+
  
- 
-def deploy(branch):
+def ssh_script(script_name, branch):
     print "deploy branch of %s" % branch
-    deploy_script = "sh deploy.sh %s" % branch
+    deploy_script = "sh %s %s" % (script_name, branch)
  
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -46,6 +65,9 @@ def deploy(branch):
     for m in msgs:
         print m,
     ssh.close()
- 
+
+def local_script(script_name, branch):
+    pass
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4040)
